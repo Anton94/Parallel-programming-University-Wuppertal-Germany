1482dc9e82e4c00ff953d4a16a400bc9c64de589
Anton94 <antonvdudov@gmail.com>
2016-04-30 18:40:40 +0200

    Done with task 2 from Block 0

--- Block0/Block0/Source.c
+++ Block0/Block0/Source.c
@@ -1,4 +1,3 @@
-//#include "stdafx.h"
 #include "mpi.h"
 #include "stdio.h"
 #include "stdlib.h"
@@ -8,30 +7,56 @@
 void task1(int argc, char* argv[])
 {
 	MPI_Init(&argc, &argv);
+
 	int rank, processesCount;
+
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &processesCount);
-	printf("Program running on %d processes, I am no process. %d", processesCount, rank);/*
-	if (rank == 0)
-	{
-		char helloStr[] = "Hello world";
-		MPI_Send(helloStr, _countof(helloStr), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+
+	printf("Program running on %d processes, I am no process. %d", processesCount, rank);
+
+	MPI_Finalize();	
+}
+
+// Task 2.
+// In process 0 set the user input.
+// Then transpit it through the process ring to all other processes, while
+// in each process double the value and output it.
+void task2(int argc, char* argv[])
+{
+	MPI_Init(&argc, &argv);
+
+	int rank, processesCount;
+	double value, computed;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &processesCount);
+	
+	if (rank == 0) // First one has to read the value from the user and send it to second one.
+	{	
+		printf("Enter a number: ");
+		fflush(stdout);
+		fscanf(stdin, "%lf", &value);
 	}
-	else if (rank == 1)
+	else // Every other process(exept 0) receives a value.
 	{
-		char helloStr[12];
-		MPI_Recv(helloStr, _countof(helloStr), MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-		printf("Rank 1 received string %s from Rank 0\n", helloStr);
-	}*/
+		MPI_Recv(&value, 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+	}
+
+	computed = value * 2;
+	printf("Process %d got %f and computed %f\n", rank, value, computed);
+
+	if (rank < processesCount - 1) // every process, exept last one, sends a computed value.
+	{
+		MPI_Send(&computed, 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
+	}
 
 	MPI_Finalize();
-	
 }
 
 int main(int argc, char* argv[])
-{
-	
-	task1(argc, argv);
+{	
+	task2(argc, argv);
 
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Parallel-programming-University-Wuppertal-Germany
 
 Parallel programming with MS-MPI and other...
+
+
+Hint: run *.exe with mpiexec -n 2 *.exe
+where *.exe is the executable file(From Debug folder for example...)
